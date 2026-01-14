9bd5f372c18961aefd9aff587d647dd294409b5e
raj1320 <ranaroshan1232005@gmail.com>
2026-01-14 22:19:50 +0530

    Task one file..

---
+++ Intro.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class Intro
+{
+	static void Main(String []args)
+	{
+		string MachineName = Environment.MachineName;
+		DateTime date = DateTime.Today;
+
+		Console.Write("===========================\n");
+		Console.WriteLine("Rana Raj");
+		Console.WriteLine("Today Date is : "+ date);
+		Console.WriteLine("Machine Name is : " + MachineName);
+        Console.Write("===========================\n");
+
+    }
+}
